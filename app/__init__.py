fd9cf5d67c342e9b45a28efd9741994e3f14f861
Orgjvr <Orgjvr@users.noreply.github.com>
2019-10-08 09:17:08 +0200

    Added some config settings

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,23 @@ from flask_socketio import SocketIO #, emit
 
 socketio = SocketIO()
 
-#app = Flask(__name__, instance_relative_config=True)
-#app.config['SECRET_KEY'] = 'secret!'
-#socketio = SocketIO(app)
+
 
 def create_app(debug=False):
     """Create an application."""
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     app.debug = debug
     app.config['SECRET_KEY'] = 'gjr39dkjn344_!67#'
+
+    # Load the default configuration
+    app.config.from_object('config.default')
+
+    # Load the configuration from the instance folder
+    app.config.from_pyfile('config.py')
+
+    # Load the file specified by the APP_CONFIG_FILE environment variable
+    # Variables defined here will override those in the default configuration
+    #app.config.from_envvar('APP_CONFIG_FILE')
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
@@ -19,20 +27,4 @@ def create_app(debug=False):
     socketio.init_app(app)
     return app
 
-
-# Load the default configuration
-#app.config.from_object('config.default')
-
-# Load the configuration from the instance folder
-#app.config.from_pyfile('config.py')
-
-# Load the file specified by the APP_CONFIG_FILE environment variable
-# Variables defined here will override those in the default configuration
-#app.config.from_envvar('APP_CONFIG_FILE')
-
-#import views
-#import pcbdrill.views
-
-#if __name__ == '__main__':
-#    socketio.run(app)
 
