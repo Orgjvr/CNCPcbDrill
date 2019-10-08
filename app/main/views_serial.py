24c88a78acd69ac3494b9b27ab596b214f23bcc7
robZeilinga <glock17ssp@gmail.com>
2019-10-08 21:58:10 +0200

    started Serial Views, added defalt read

---
+++ app/main/views_serial.py
@@ -0,0 +1,30 @@
+from flask import session, redirect, url_for, render_template, request, Response
+from . import main
+#from .forms import LoginForm
+import json
+import logging
+from flask import current_app as app
+
+
+#TODO: add send of json camera list from GetCameras
+#TODO: add dynamic route to activate camera /c/2 
+
+@main.route('/getSerialDefault')
+def getSerialDefault(): 
+    logging.basicConfig(level=logging.DEBUG)
+
+    logging.info("hello world")
+    global baudRate
+    baudRate = app.config['SERIALPORTBAUDRATE']
+    logging.info("baudRate = " + str(baudRate))
+    
+    return Response("Baudrate = " + str(baudRate))
+
+@main.route('/getSerialPorts')
+def getSerialPorst():
+    global sPorts
+    sPorts = serialPort.GetSerialPorts()
+    return Response(json.dumps(sPorts))
+
+    #return "Camera %d activated"% (camIndex)
+
